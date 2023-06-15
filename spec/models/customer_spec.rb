9d10bf9b5150ce00bf7c0cde1574fdfad174792e
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-06-15 17:29:22 -0300

    test: factory bot

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   fixtures :all
 
-  it "Creat a Customer" do
+  it "#full name" do
     customer = create(:customer)
-    expect(customer.full_name).to eq("Sr. Jackson Pires")
-    end
+    expect(customer.full_name).to start_with("Sr. ")
+  end
+
+  it {expect{create(:customer)}.to change{Customer.all.size}.by(1)}
 end
