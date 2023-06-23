3b1c6f727cd81ab2bbead14581e3bd5dbde7189e
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-06-23 17:12:32 -0300

    test: product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'with description, price and category' do
+    product = create(:product)
+    expect(product).to be_valid
+  end
+
+  it 'has no description, is invalid' do
+    product = build(:product, description: nil)
+    product.valid?
+    expect(product.errors[:description]).to include("can't be blank")
+  end
 end
